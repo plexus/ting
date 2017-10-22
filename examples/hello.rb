9fa65f370be324cc96e537ba3e7f298d905c0f00
Julian Raschke <julian@raschke.de>
2017-10-22 19:37:13 +0200

    Fix examples/hello.rb

--- examples/hello.rb
+++ examples/hello.rb
@@ -1,11 +1,11 @@
 $: << File.join(File.dirname(__FILE__), '../lib')
 
-require 'pinyin'
+require 'ting'
 
-conv1 = Pinyin::Converter.new(:hanyu, :numbers, :wadegiles, :accents)
-conv2 = Pinyin::Converter.new(:wadegiles, :accents, :zhuyin, :marks)
+conv1 = Ting::Converter.new(:hanyu, :numbers, :wadegiles, :accents)
+conv2 = Ting::Converter.new(:wadegiles, :accents, :zhuyin, :marks)
 
-pinyin    = 'wo3 de2 peng2 you3 shi4 dai4 fu'
+pinyin    = 'wo3 de peng2 you3 shi4 dai4 fu'
 wadegiles = conv1 << pinyin
 zhuyin    = conv2 << wadegiles
 
