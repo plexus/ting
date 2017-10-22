3163bbed4bc3e511474050548dcf4e7754d160b6
Julian Raschke <julian@raschke.de>
2017-10-22 18:04:57 +0200

    Fix rspec warning about old vs new syntax

--- spec/ting_spec.rb
+++ spec/ting_spec.rb
@@ -6,14 +6,14 @@ describe Ting do
   let(:bopomofo) { 'ㄉㄠˋ ㄎㄜˇ ㄉㄠˋ ㄈㄟ ㄔㄤˊ ㄉㄠˋ'.force_encoding('UTF-8') }
 
   it 'should convert from Hany Pinyin to Bopomofo' do
-    Ting.from(:hanyu, :numbers).to(:zhuyin, :marks).convert(pinyin).should == bopomofo
+    expect(Ting.from(:hanyu, :numbers).to(:zhuyin, :marks).convert(pinyin)).to eq(bopomofo)
   end
 
-  it "should parse" do
-    Ting::Reader.new(:hanyu, :numbers).parse('Bei3').first.should == Ting::Syllable.new( Ting::Initial::Bo, Ting::Final::Ei, 3, true )
+  it 'should parse' do
+    expect(Ting::Reader.new(:hanyu, :numbers).parse('Bei3').first).to eq(Ting::Syllable.new( Ting::Initial::Bo, Ting::Final::Ei, 3, true ))
   end
 
   it 'should respect capitalization' do
-    Ting.from(:hanyu, :numbers).to(:hanyu, :accents).convert('Bei3 jing1').should == 'Běi jīng'
+    expect(Ting.from(:hanyu, :numbers).to(:hanyu, :accents).convert('Bei3 jing1')).to eq('Běi jīng')
   end
 end
