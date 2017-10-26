a1761b7a3f76327cfb7494ce57297b298a38564a
Arne Brasseur <arne@arnebrasseur.net>
2017-10-26 16:11:32 +0200

    Fix the Palladius version of "hui"
    Wikipedia seems to indicate this is the more common form.

--- spec/palladius_spec.rb
+++ spec/palladius_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe 'palladius' do
   it 'should convert from Hanyu pinyin to Palladius' do
     expect(converter.call('you3 peng2 zi4 yuan2 fang2 lai2')).to eql 'ю пэн цзы юaнь фан лай'
   end
+
+  it 'avoids the word for "penis"' do
+    expect(converter.call('hui4')).to eql 'хуэй'
+  end
 end
